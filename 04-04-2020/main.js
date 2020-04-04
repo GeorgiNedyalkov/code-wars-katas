9c5d651fdb8dd593796c814baeed4e657facb8eb
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-04 11:57:09 +0300

    First kata for the day!

---
+++ 04-04-2020/main.js
@@ -0,0 +1,33 @@
+String.prototype.toJadenCase = function () {
+    console.log(this)
+    
+    let jadenCase = '';    
+    let capitalize = true;
+    
+    for (let i = 0; i < this.length; i++) {
+        const letter = this[i];
+
+        if (letter === ' ') {
+            capitalize = true;
+            jadenCase += ' ';
+        } else {
+            if (capitalize) {
+                jadenCase += letter.toUpperCase();
+                capitalize = false;
+            } else {
+                jadenCase += letter.toLowerCase();
+            }
+        }        
+    }
+
+    return jadenCase;
+  };
+
+var str = "How can mirrors be real if our eyes aren't real";
+var jadenStr = str.toJadenCase();
+console.log(jadenStr);    
+
+
+// split the string on spaces
+// capitalize the first letter 
+// join them back together
