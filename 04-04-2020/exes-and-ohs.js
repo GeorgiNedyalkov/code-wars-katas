3cb1d78757b67b0906828c4b5066c29a3136f0e5
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-04 12:35:44 +0300

    New kata with a first solution

---
+++ 04-04-2020/exes-and-ohs.js
@@ -0,0 +1,27 @@
+function XO(str) {   
+    let xAmount = 0;
+    let oAmount = 0;
+    let same = true; 
+    
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'x' || str[i] === 'X') {
+            xAmount++;
+        } else if (str[i] === 'o' || str[i] === 'O') {
+           oAmount++; 
+        }         
+    }
+
+    if (xAmount === oAmount) {
+        return same;
+    } else {
+        return !same; 
+    }    
+}
+
+
+console.log(XO("ooxx")) // true
+console.log(XO("xooxx")) // false
+console.log(XO("ooxXm")) // true
+console.log(XO("zpzpzpp")) // true // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo")) // false
