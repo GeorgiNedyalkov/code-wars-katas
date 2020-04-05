21a0a6adf68200b9f474fe7394e8ca90e7d366c4
Georgi Nedyalkov <georgi@zoom.tech>
2020-04-05 12:51:44 +0300

    solving algorythm katas

---
+++ 05-04-2020/are-numbers-ascending.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+
+function inAscOrder(arr) {
+    // are numbers in the arr in an ascending order
+    for (let i = 0; i < arr.length; i++) {
+        let number = arr[i];
+        
+        
+    }
+    
+    
+
+    
+  }
+
+console.log(inAscOrder([1, 2, 4, 7, 19]));
+console.log(inAscOrder([1, 2, 3, 4, 5]));
+console.log(inAscOrder([1, 6, 10, 18, 2]));
