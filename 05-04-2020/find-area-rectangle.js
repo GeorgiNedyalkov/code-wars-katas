f0cce2c4922314ca27420b079ebe5f2c05218bc3
Georgi Nedyalkov <georgi@zoom.tech>
2020-04-05 12:21:41 +0300

    First kata of the day! #codingSamurai

---
+++ 05-04-2020/find-area-rectangle.js
@@ -0,0 +1,32 @@
+/*
+    Find the area of a rectangle when provided with:
+     one diagonal - d - and one side - l of the rectangle. 
+        If the input diagonal is less than or equal to the length of the side, 
+            return "Not a rectangle". 
+        If the resultant area has decimals round it to two places.
+
+        Solution:
+        a) we find w given d and l 
+        b) we find area A from a)
+        c) check to see if it a decimal with % - to limit the places after point to two.
+*/
+
+function area(d,l) {    
+    if (d <= l) {
+        return "Not a rectangle";
+    }    
+
+    let w = Math.sqrt(Math.pow(d, 2) - Math.pow(l, 2));
+    let rectangleA = w * l;
+
+    if (rectangleA % 1 != 0) {
+        let result =  rectangleA.toFixed(2);
+        return +result;
+    }
+
+    return rectangleA;
+}
+
+console.log(area(5, 4));  // 12
+console.log(area(10, 6)); // 48
+console.log(area(12, 5)); // 54.54
