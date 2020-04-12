3ed721065fdcc14528786c26a73524fe7312e209
Georgi Nedyalkov <georgi@zoom.tech>
2020-04-12 21:03:11 +0300

    Added new implementation and unfinished katas

--- 12-04-2020/calculator.js
+++ 12-04-2020/calculator.js
@@ -19,10 +19,19 @@ function calculate(num1, operation, num2) {
     return result;     
 }
 
-console.log(calculate(3.2, "+", 8));
-console.log(calculate(3.2, "-", 8));
-console.log(calculate(3.2, "/", 0));
-console.log(calculate(3.2, "*", 8));
-console.log(calculate(3.2, "*", 8));
-console.log(calculate(-3, "w", 0));
+console.log(calculator(3.2, "+", 8));
+console.log(calculator(3.2, "-", 8));
+console.log(calculator(3.2, "/", 0));
+console.log(calculator(3.2, "*", 8));
+console.log(calculator(3.2, "*", 8));
+console.log(calculator(-3, "w", 0));
 
+function calculator(num1, operation, num2) {
+    var ops = {
+        "+": function(x, y) { return x + y; },
+        "-": function(x, y) { return x - y; },
+        "*": function(x, y) { return x * y; },
+        "/": function(x, y) { return y === 0 ? null : x / y; },
+    };
+    return(ops[operation] || function() { return null; })(num1, num2);
+}
