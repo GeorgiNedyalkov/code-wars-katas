e9d09e3fd59e8fdf385239df639b178d7e33b5ea
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-13 11:21:36 +0300

    Add a new kata - Array Leaders

---
+++ 13-04-2020/array-leaders.js
@@ -0,0 +1,39 @@
+function arrayLeaders(arr) {
+
+    let leaders = [];
+    let arrayLength = arr.length;
+
+    for (let i = 0; i < arrayLength; i++) {
+        
+        let currentNumber = arr.shift();
+        let sumArrNumbers = arr.reduce((a, b) => a + b, 0);
+
+        if (currentNumber > sumArrNumbers) {
+            leaders.push(currentNumber);
+        }
+    }
+    return leaders;
+}
+
+
+console.log(arrayLeaders([1, 2, 3, 4, 0]));
+
+
+
+/* function arrLeaders(numbers) {
+
+    let leaders = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        let sum = 0;
+        let currentNumber = numbers[i];
+
+        for (let j =i+1; j < numbers.length; j++) {
+            sum += numbers[j];            
+        }
+
+        if (currentNumber > sum) {
+            leaders.push(currentNumber);
+        }        
+    }
+} */
